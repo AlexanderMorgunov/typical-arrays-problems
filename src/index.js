b09f74594c0462c3c9b61f1d2dba90fb92cf00ee
AexanderMorgunov <morgunowaleks@yandex.ru>
2021-08-20 00:12:32 +0300

    feat create a function of search min,max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array==undefined||array==NaN||array==null||!array) return 0;
+    let min=array[0];
+    array.forEach(element => {
+        if(element<min)min=element;
+    });
+    if (min==undefined) return 0;
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array)return 0;
+    let max=array[0];
+    array.forEach(element => {
+        element>max?max=element:max;
+    });
+    if (max==undefined) return 0;
+  return max;
 }
-
 exports.avg = function avg (array) {
-  return 0;
+    if(array==undefined||array==NaN||array==null||!array||array.length==0) return 0;
+    let count=0;
+    array.forEach(element => {
+        count+=element;
+    });
+    return count/array.length.toFixed(2);
 }
